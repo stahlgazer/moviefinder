726dde0cab10a51a411d6e1a3692f455e3cfe449
Gavin S <stahlgavin@gmail.com>
2020-10-16 18:14:17 -0700

    data displaying, basic components built

---
+++ src/components/PopularMovies.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import PopularCard from "./PopularCards";
+
+export default function PopularMovies() {
+  const [popular, setPopular] = useState([]);
+  useEffect(() => {
+    getPopularMovies();
+  }, []);
+  const getPopularMovies = async () => {
+    const url = `https://api.themoviedb.org/3/movie/popular?api_key=669c7025d14663e5ee3c8ac1e3e2945e&language=en-US`;
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      setPopular(data.results);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  console.log(popular);
+
+  return (
+    <div>
+      {popular
+        .filter((movie) => movie.vote_average >= 7)
+        .map((movie) => {
+          return <PopularCard movie={movie} key={movie.id} />;
+        })}
+    </div>
+  );
+}
---
+++ src/components/SearchMovies.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import SearchCards from "./SearchCards.js";
+
+export default function SearchMovies() {
+  const [search, setSearch] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  const searchMovies = async (e) => {
+    e.preventDefault();
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=669c7025d14663e5ee3c8ac1e3e2945e&language=en-US&query=${search}`;
+
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      setMovies(data.results);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <>
+      <form className="form" onSubmit={searchMovies}>
+        <label className="label" htmlFor="search">
+          Movie Name
+        </label>
+        <input
+          className="input"
+          type="text"
+          name="search"
+          placeholder="i.e. Jurassic Park"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button className="button" type="submit" disabled={!search}>
+          Search
+        </button>
+      </form>
+      <div className="card-list">
+        {movies
+          .filter((movie) => movie.poster_path)
+          .map((movie) => (
+            <SearchCards movie={movie} key={movie.id} />
+          ))}
+      </div>
+    </>
+  );
+}
